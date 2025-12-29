caa4c8f1452fb96bc28b146a79e367257158b204
Satwikloka-dev <satwik@zithara.in>
2025-12-29 13:48:48 +0530

    logo added

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-
+import { useTheme } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
-  logo: <span>Zithara Docs</span>,
+  logo: (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+      <img
+        src="/logo.png" // your round logo in public folder
+        alt="Zithara"
+        style={{
+          height: '60px',
+          width: '60px',
+          borderRadius: '50%',
+          objectFit: 'cover',
+        }}
+      />
+      <span style={{ fontWeight: 'bold', fontSize: '1.1rem' }}>Docs</span>
+    </div>
+  ),
+
+
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Zithara-in',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Zithara-in',
   footer: {
     text: 'Zithara Documentation',
   },
